628a39b8b83371a8093a68288937b6f57d0c8ff6
hacknus <l_stoeckli@bluewin.ch>
2022-12-12 16:02:57 +0100

    i'm stuck

--- src/days/day_12.rs
+++ src/days/day_12.rs
@@ -1,54 +1,62 @@
+use num::integer::Roots;
 use crate::io::read_file_lines;
 use crate::problem::Problem;
 
 pub struct DayTwelve {}
 
-pub fn walk(grid: &Vec<Vec<usize>>, index: &[usize; 2], last: &[usize; 2], target: &[usize; 2]) -> u32 {
-    let mut walk_counter = 0;
-    if index[0] == target[0] && index[1] == target[1] {
-        return walk_counter;
-    }
-    println!("index of walk {index:?}, index of last {last:?}");
-    let current_position = grid[index[0]][index[1]];
-    if index[0] > 0 {
-        let left = [index[0] - 1, index[1]];
-        if left[0] != last[0] || left[1] != last[1] {
-            if (current_position as i32 - grid[left[0]][left[1]] as i32).abs() <= 1 {
-                println!("moving left");
-                walk_counter += walk(grid, &left, index, target);
-            }
-        }
-    }
-    if index[0] < grid.len() - 1 {
-        let right = [index[0] + 1, index[1]];
-        if right[0] != last[0] || right[1] != last[1] {
-            if (current_position as i32 - grid[right[0]][right[1]] as i32).abs() <= 1 {
-                println!("moving right");
-                walk_counter += walk(grid, &right, index, target);
-            }
-        }
-    }
-    if index[1] < grid[0].len() - 1 {
-        let up = [index[0], index[1] + 1];
-        if up[0] != last[0] || up[1] != last[1] {
-            if (current_position as i32 - grid[up[0]][up[1]] as i32).abs() <= 1 {
-                println!("moving up");
-                walk_counter += walk(grid, &up, index, target);
-            }
-        }
-    }
-    if index[1] > 0 {
-        let down = [index[0], index[1] - 1];
-        if down[0] != last[0] || down[1] != last[1] {
-            if (current_position as i32 - grid[down[0]][down[1]] as i32).abs() <= 1 {
-                println!("moving down");
-                walk_counter += walk(grid, &down, index, target);
-            }
-        }
-    }
-    walk_counter
+pub enum Direction {
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN,
 }
 
+pub fn get_weight(grid: &Vec<Vec<u32>>, location: &[usize; 2]) -> Vec<Vec<f32>> {
+    let current_altitude = grid[location[1]][location[0]];
+    let mut weight = vec![vec![1.0; grid[0].len()]; grid.len()];
+    for altitude in (current_altitude..25).rev() {
+        for y in 0..grid.len() {
+            for x in 0..grid[0].len() {
+                if altitude == grid[y][x] && [x, y] != *location && grid[y][x] - current_altitude <= 1 {
+                    weight[y][x] += altitude as f32 / ((x as i32 - location[0] as i32).pow(2) + (y as i32 - location[1] as i32).pow(2)).sqrt() as f32;
+                }
+            }
+        }
+    }
+    weight
+}
+
+pub fn make_move(weights: &Vec<Vec<f32>>, location: &[usize; 2]) -> Direction {
+    let mut up = 0;
+    let mut down = 0;
+    let mut left = 0;
+    let mut right = 0;
+    for x in 0..weights[0].len() {
+        for y in 0..weights.len() {
+            if x < location[0] {
+                left += (weights[y][x]*100.0) as u32;
+            } else if x > location[0] {
+                right += (weights[y][x]*100.0) as u32;
+            }
+            if y < location[1] {
+                down += (weights[y][x]*100.0) as u32;
+            } else if y > location[1] {
+                up += (weights[y][x]*100.0) as u32;
+            }
+        }
+    }
+    if [up, down, right, left].iter().max().unwrap() == &up {
+        Direction::UP
+    } else if [up, down, right, left].iter().max().unwrap() == &down {
+        Direction::DOWN
+    } else if [up, down, right, left].iter().max().unwrap() == &left {
+        Direction::LEFT
+    } else if [up, down, right, left].iter().max().unwrap() == &right {
+        Direction::RIGHT
+    } else {
+        Direction::UP
+    }
+}
 
 impl Problem for DayTwelve {
     fn part_one(&self, input: &str) -> String {
@@ -69,24 +77,59 @@ impl Problem for DayTwelve {
                         } else if s == 'E' {
                             target = [x, y];
                             x += 1;
-                            25
+                            letters.len() as u32
                         } else {
                             panic!("error: {s}");
                         }
                     }
                     Some(si) => {
                         x += 1;
-                        si
+                        si as u32
                     }
                 }
-            }).collect::<Vec<usize>>();
+            }).collect::<Vec<u32>>();
             grid.push(row);
         }
         let current_height = 0;
         println!("{grid:?}");
         println!("{initial:?}");
         println!("{target:?}");
-        let counter = walk(&grid, &initial, &initial, &target);
+        for line in grid.iter() {
+            println!("{line:?}");
+        }
+        let mut counter = 0;
+        loop {
+            if initial == target {
+                break;
+            }
+            let weights = get_weight(&grid, &initial);
+            for line in weights.iter() {
+                println!("{line:?}");
+            }
+            let next_move = make_move(&weights, &initial);
+            counter += 1;
+            match next_move {
+                Direction::LEFT => {
+                    println!("going left");
+                    initial[0] -= 1;
+                }
+                Direction::RIGHT => {
+                    println!("going right");
+                    initial[0] += 1;
+                }
+                Direction::UP => {
+                    println!("going up");
+                    initial[1] += 1;
+                }
+                Direction::DOWN => {
+                    println!("going down");
+                    initial[1] -= 1;
+                }
+            }
+        }
+
+
+
         format!("{}", counter)
     }
 
